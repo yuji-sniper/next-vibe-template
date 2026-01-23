0767b534eac04332651adae8f493ff114bbd3d67
吉村直人 <ahowateto123@gmail.com>
2026-01-24 08:20:38 +0900

    Refactor getAuthUserQuery to accept optional parameters for error handling and return undefined authUser when orError is false

--- src/features/auth/queries/get-auth-user.ts
+++ src/features/auth/queries/get-auth-user.ts
@@ -2,14 +2,22 @@ import { getAuthUserAction } from "@/backend"
 import { ServerError } from "@/utils/error"
 import type { AuthUser } from "../types/auth-user"
 
-export type GetAuthUserQuery = () => Promise<{
-  authUser: AuthUser
+export type GetAuthUserQuery = (params: { orError?: boolean }) => Promise<{
+  authUser: AuthUser | undefined
 }>
 
-export const getAuthUserQuery: GetAuthUserQuery = async () => {
+export const getAuthUserQuery: GetAuthUserQuery = async ({
+  orError = true
+}) => {
   const res = await getAuthUserAction()
 
   if (!res.ok) {
+    if (!orError) {
+      return {
+        authUser: undefined
+      }
+    }
+
     throw new ServerError(
       res.error.code,
       res.error.status,
