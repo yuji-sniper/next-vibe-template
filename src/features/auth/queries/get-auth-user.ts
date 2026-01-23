5e68dfa3de3b33e77bfd92833505f0f7f144e6b1
吉村直人 <ahowateto123@gmail.com>
2026-01-24 03:51:14 +0900

    Refactor getAuthUserQuery to remove redirect logic and simplify parameters

--- src/features/auth/queries/get-auth-user.ts
+++ src/features/auth/queries/get-auth-user.ts
@@ -1,23 +1,15 @@
-import { redirect } from "next/navigation"
 import { getAuthUserAction } from "@/backend"
 import { ServerError } from "@/utils/error"
 import type { AuthUser } from "../types/auth-user"
 
-export type GetAuthUserQuery = (params: {
-  redirectIfUnauthorized?: boolean
-}) => Promise<{
+export type GetAuthUserQuery = () => Promise<{
   authUser: AuthUser
 }>
 
-export const getAuthUserQuery: GetAuthUserQuery = async ({
-  redirectIfUnauthorized = false
-}) => {
+export const getAuthUserQuery: GetAuthUserQuery = async () => {
   const res = await getAuthUserAction()
 
   if (!res.ok) {
-    if (redirectIfUnauthorized) {
-      redirect("/sign-in")
-    }
     throw new ServerError(
       res.error.code,
       res.error.status,
