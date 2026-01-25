efbe43d87c97faeab6e4a54e50de93590fbf55ae
吉村直人 <ahowateto123@gmail.com>
2026-01-25 12:03:22 +0900

    Add richColors prop to Sonner component for enhanced notification styling

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -17,6 +17,7 @@ const Toaster = ({ ...props }: ToasterProps) => {
     <Sonner
       theme={theme as ToasterProps["theme"]}
       className="toaster group"
+      richColors
       icons={{
         success: <CircleCheckIcon className="size-4" />,
         info: <InfoIcon className="size-4" />,
