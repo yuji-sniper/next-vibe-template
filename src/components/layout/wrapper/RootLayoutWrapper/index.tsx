e7adc94a045b6dc232dd8455e7230919053ed7d9
吉村直人 <ahowateto123@gmail.com>
2026-01-30 20:54:58 +0900

    Remove RootLayoutWrapper component as it is no longer needed in the layout structure.

--- src/components/layout/wrapper/RootLayoutWrapper/index.tsx
+++
@@ -1,8 +0,0 @@
-"use client"
-
-import type { PropsWithChildren } from "react"
-import { QueryProvider } from "@/providers/QueryProvider"
-
-export const RootLayoutWrapper = ({ children }: PropsWithChildren) => {
-  return <QueryProvider>{children}</QueryProvider>
-}
