bb5b41f11c71442e53826fed024d595e604a7b5c
吉村直人 <ahowateto123@gmail.com>
2026-01-31 16:51:17 +0900

    Add Terraform configuration files for development environment

---
+++ terraform/dev/locals.tf
@@ -0,0 +1,6 @@
+locals {
+  project            = "next-vibe-template-dev"
+  region             = "ap-northeast-1"
+  domain             = "next-vibe-template.com"
+  availability_zones = ["ap-northeast-1a"]
+}
