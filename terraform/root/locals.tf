86dde6a537cc19075162199ec72907f6a1f2dbde
吉村直人 <ahowateto123@gmail.com>
2026-02-02 20:28:47 +0900

    Remove Terraform configuration files and versioning for the project

--- terraform/root/locals.tf
+++
@@ -1,6 +0,0 @@
-locals {
-  project            = "next-vibe-template-dev"
-  region             = "ap-northeast-1"
-  domain             = "next-vibe-template.com"
-  availability_zones = ["ap-northeast-1a"]
-}
