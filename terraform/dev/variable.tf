4a518013c7c40d502f410b66ad46ced4d80b6e7b
吉村直人 <ahowateto123@gmail.com>
2026-02-03 19:22:42 +0900

    Remove email processing Lambda function and associated resources

--- terraform/dev/variable.tf
+++ terraform/dev/variable.tf
@@ -1,23 +1,3 @@
-variable "database_host" {
-  type        = string
-  description = "The host of the database"
-}
-
-variable "database_port" {
-  type        = number
-  description = "The port of the database"
-}
-
-variable "database_user" {
-  type        = string
-  description = "The user of the database"
-}
-
-variable "database_name" {
-  type        = string
-  description = "The name of the database"
-}
-
 variable "vercel_cname_target" {
   type        = string
   description = "The CNAME target of the Vercel deployment"
--- terraform/prod/variable.tf
+++ terraform/prod/variable.tf
@@ -7,23 +7,3 @@ variable "dev_name_servers" {
   type        = list(string)
   description = "The name servers of the development environment"
 }
-
-variable "database_host" {
-  type        = string
-  description = "The host of the database"
-}
-
-variable "database_port" {
-  type        = number
-  description = "The port of the database"
-}
-
-variable "database_user" {
-  type        = string
-  description = "The user of the database"
-}
-
-variable "database_name" {
-  type        = string
-  description = "The name of the database"
-}
